01931dd808a7c83be262daae3febd15e6a8e7e56
Levi <dancs.levente@ckik.hu>
2024-11-18 10:36:33 +0100

    Levi második kommit: 1a próbálkozás

--- IKTProjektI_DLVG.py
+++ IKTProjektI_DLVG.py
@@ -8,3 +8,19 @@ i=0
 for i in range(0,n,1):
     t.append(random.randint(-10,10))
     i+=1
+
+#1a
+
+t_abs=[]
+
+c=0
+
+for c in range(0,n,1):
+    if t[c]<0:
+        t_abs.append(abs(i))
+    else:
+        t_abs.append(i)
+    c+=1    
+
+print(t_abs)
+#print(t/t_abs*10000/100, "százalékkal ment többet.")
