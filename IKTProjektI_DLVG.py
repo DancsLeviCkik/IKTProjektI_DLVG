b4547f002a52393d0e011018fe4c57c9b5b54ae5
Levi <dancs.levente@ckik.hu>
2024-11-26 13:58:55 +0100

    1b kész

--- IKTProjektI_DLVG.py
+++ IKTProjektI_DLVG.py
@@ -32,5 +32,23 @@ lepesek_abszolut=0
 for b in range(0,n,1):
     lepesek_abszolut+=t_abs[b]
 
-print(round(((lepesek_abszolut-lepesek)/lepesek_abszolut*100),2), "százalékkal ment többet.")
+print(f'{round(((lepesek_abszolut-lepesek)/lepesek_abszolut*100),2)} százalékkal ment többet')
 print(lepesek, lepesek_abszolut)
+
+#1b
+
+t_lepessorozat=[]
+
+d=0
+
+haladas=0
+
+for d in range(0,n,1):
+    if t[d] >= 0:
+        haladas+=1
+    else:
+        t_lepessorozat.append(haladas)
+        haladas-=haladas
+
+print(max(t_lepessorozat))
+print(t)
