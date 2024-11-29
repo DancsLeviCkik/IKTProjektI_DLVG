7cef4cba0c0913e74515c8d765152afca4d9e22f
Levi <dancs.levente@ckik.hu>
2024-11-29 08:41:20 +0100

    1b függvénymentesítve

--- IKTProjektI_DLVG.py
+++ IKTProjektI_DLVG.py
@@ -12,43 +12,53 @@ for i in range(0,n,1):
 
 t_abs=[]
 
-c=0
+i=0
 
-for c in range(0,n,1):
-    if t[c]<0:
-        t_abs.append(abs(t[c]))
+for i in range(0,n,1):
+    if t[i]<0:
+        t_abs.append(abs(t[i]))
     else:
-        t_abs.append(t[c])
+        t_abs.append(t[i])
 
-a=0
+i=0
 lepesek=0
 
-for a in range(0,n,1):
-    lepesek+=t[a]
+for i in range(0,n,1):
+    lepesek+=t[i]
 
-b=0
+i=0
 lepesek_abszolut=0
 
-for b in range(0,n,1):
-    lepesek_abszolut+=t_abs[b]
+for i in range(0,n,1):
+    lepesek_abszolut+=t_abs[i]
 
 print(f'{round(((lepesek_abszolut-lepesek)/lepesek_abszolut*100),2)} százalékkal ment többet')
-print(lepesek, lepesek_abszolut)
 
 #1b
 
 t_lepessorozat=[]
 
-d=0
+i=0
 
 haladas=0
 
-for d in range(0,n,1):
-    if t[d] >= 0:
+for i in range(0,n,1):
+    if t[i] >= 0:
         haladas+=1
     else:
         t_lepessorozat.append(haladas)
         haladas-=haladas
 
-print(max(t_lepessorozat))
-print(t)
+i=0                
+
+max=t_lepessorozat[0]
+
+for i in range(len(t_lepessorozat)):
+    if max<t_lepessorozat[i]:
+        max=t_lepessorozat[i]
+
+print(max)
+print(t_lepessorozat)
+
+#1c
+
