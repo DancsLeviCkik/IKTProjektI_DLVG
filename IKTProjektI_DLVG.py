258299eda84b3f652ba6d60d622f89a6a9a5dbde
Levi <dancs.levente@ckik.hu>
2024-12-02 09:00:57 +0100

    indexek javítva

--- IKTProjektI_DLVG.py
+++ IKTProjektI_DLVG.py
@@ -26,11 +26,11 @@ lepesek=0
 for i3 in range(0,n,1):
     lepesek+=t[i3]
 
-i3=0
+i4=0
 lepesek_abszolut=0
 
-for i3 in range(0,n,1):
-    lepesek_abszolut+=t_abs[i3]
+for i4 in range(0,n,1):
+    lepesek_abszolut+=t_abs[i4]
 
 print(f'{round(((lepesek_abszolut-lepesek)/lepesek_abszolut*100),2)} százalékkal ment többet')
 
@@ -38,24 +38,24 @@ print(f'{round(((lepesek_abszolut-lepesek)/lepesek_abszolut*100),2)} százalékk
 
 t_lepessorozat=[]
 
-i4=0
+i5=0
 
 haladas=0
 
-for i4 in range(0,n,1):
-    if t[i4] >= 0:
+for i5 in range(0,n,1):
+    if t[i5] >= 0:
         haladas+=1
     else:
         t_lepessorozat.append(haladas)
         haladas-=haladas
 
-i5=0                
+i6=0                
 
 max=t_lepessorozat[0]
 
-for i5 in range(len(t_lepessorozat)):
-    if max<t_lepessorozat[i5]:
-        max=t_lepessorozat[i5]
+for i6 in range(len(t_lepessorozat)):
+    if max<t_lepessorozat[i6]:
+        max=t_lepessorozat[i6]
 
 print(max)
 
@@ -63,17 +63,17 @@ print(max)
 
 maxlepes=t_lepessorozat[0]
 
-i6=0
-
-for i6 in range(len(t_abs)):
-    if maxlepes<t_abs[i6]:
-        maxlepes=t_abs[i6]
-
 i7=0
-maxlepes_szam=0
 
 for i7 in range(len(t_abs)):
-    if t_abs[i7]==maxlepes:
+    if maxlepes<t_abs[i7]:
+        maxlepes=t_abs[i7]
+
+i8=0
+maxlepes_szam=0
+
+for i8 in range(len(t_abs)):
+    if t_abs[i8]==maxlepes:
         maxlepes_szam+=1
 
 print(f'{(maxlepes_szam)} db és {maxlepes} láb volt.')
